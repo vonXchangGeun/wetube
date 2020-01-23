170ca8baa883f7aad359aeae3314d43c3996c515
김창근 <Tonio@gimchang-geun-ui-MacBook-Pro.local>
2020-01-23 11:15:49 +0900

    Add Server Handler

--- index.js
+++ index.js
@@ -7,4 +7,17 @@ function handleListening() {
   console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+  console.log(req);
+  res.send("Hi Server?");
+}
+
+function handleProfile(req, res) {
+  res.send("You`re on my Profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
