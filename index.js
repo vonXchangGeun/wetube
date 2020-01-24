d9d706974e7980b537d6ce07f18615dfd8c123ac
vonXchangGeun <bunker1031@gmail.com>
2020-01-24 22:35:19 +0900

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = 4000;
 const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
 
-const handleHome = (req, res) => res.send("Hi Server?");
+const handleHome = (req, res) => res.send("Test Server~");
 
 const handleProfile = (req, res) => res.send("You`re on my Profile");
 
