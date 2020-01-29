79d2cd2ddbe01f5949f8a8740e35392d9efdf689
vonXchangGeun <bunker1031@gmail.com>
2020-01-29 22:20:02 +0900

    Add etc Routers

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -2,4 +2,10 @@ import express from "express";
 
 const videoRouter = express.Router();
 
+videoRouter.get(routes.video, (req, res) => res.send("Videos"));
+videoRouter.get(routes.upload, (req, res) => res.send("Upload"));
+videoRouter.get(routes.videoDetail, (req, res) => res.send("VideoDetail"));
+videoRouter.get(routes.editVideo, (req, res) => res.send("EditVideo"));
+videoRouter.get(routes.deleteVideo, (req, res) => res.send("DeleteVideo"));
+
 export default videoRouter;
