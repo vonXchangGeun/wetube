2533b8752ec69d9234b4b4af3b0d8581c9a51cc4
vonXchangGeun <bunker1031@gmail.com>
2020-01-30 23:13:35 +0900

    change message

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -2,10 +2,10 @@ import express from "express";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.video, (req, res) => res.send("Videos"));
-videoRouter.get(routes.upload, (req, res) => res.send("Upload"));
-videoRouter.get(routes.videoDetail, (req, res) => res.send("VideoDetail"));
-videoRouter.get(routes.editVideo, (req, res) => res.send("EditVideo"));
-videoRouter.get(routes.deleteVideo, (req, res) => res.send("DeleteVideo"));
+videoRouter.get(routes.video, (req, res) => res.send("Videos page"));
+videoRouter.get(routes.upload, (req, res) => res.send("Upload page"));
+videoRouter.get(routes.videoDetail, (req, res) => res.send("VideoDetail page"));
+videoRouter.get(routes.editVideo, (req, res) => res.send("EditVideo page"));
+videoRouter.get(routes.deleteVideo, (req, res) => res.send("DeleteVideo page"));
 
 export default videoRouter;
